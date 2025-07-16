a2f3a53b35c4ad0e09398fc57739074954736c55
Moussa Hassana <moussahassana42@gmail.com>
2025-07-17 00:38:27 +0100

    feat(cache): add Redis caching and .env config

--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.decorators.cache import cache_page
+from django.http import JsonResponse
+from .models import Property
 
-# Create your views here.
+@cache_page(60 * 15)  # Cache for 15 minutes
+def property_list(request):
+    properties = list(Property.objects.values())
+    return JsonResponse(properties, safe=False)
