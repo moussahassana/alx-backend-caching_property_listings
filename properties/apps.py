54b30dfe60697cc96244a71a698b59aeaaaaf3f9
Moussa Hassana <moussahassana42@gmail.com>
2025-08-13 12:47:56 +0100

    feat: add Django signals to invalidate 'all_properties' cache on Property save/delete and wire app config

--- properties/apps.py
+++ properties/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
 
-
 class PropertiesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'properties'
+
+    def ready(self):
+        # Import signals so receivers are registered when the app loads
+        from . import signals  # noqa: F401
